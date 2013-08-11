476d4301b179a165edff3ec96f2b8ad054dc683c
Trevor Wilson <wilsontr@gmail.com>
2013-08-11 15:39:18 -0400

    Drop entry meta

--- single.php
+++ single.php
@@ -20,10 +20,6 @@ function single_album_scripts() {
 
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 
-		
-		<div class="entry-meta">
-		</div><!-- .entry-meta -->
-
 		<div class="entry-content">
 		
 			<?php the_content(); ?>
