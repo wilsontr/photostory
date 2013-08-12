23f0eeccc8951a4c943b660fe11a0131dc82197d
Trevor Wilson <wilsontr@gmail.com>
2013-08-11 20:52:00 -0400

    Make album list more responsive to different image heights

--- albums.php
+++ albums.php
@@ -21,22 +21,46 @@ $query = new WP_Query($attrs);
 
 $posts = $query->get_posts();
 
+$num_columns = 3;
+$columns = array();
+$count = count($posts);
+$per_column = $count / $num_columns;
+$remainder = $count % $num_columns;
+
+$idx = 0;
 foreach ( $posts as $post ) {
-	$post_img = get_the_post_thumbnail($post->ID, 'album-list');
-	$permalink = get_permalink($post->ID);
+	if ( !$columns[$idx] )
+		$columns[$idx] = array();
+	array_push($columns[$idx], $post);
+	$idx++;
+	if ( $idx == $num_columns ) 
+		$idx = 0;
+}
+
+?>
+	<div class="pure-g-r">
+<?php 
+foreach ( $columns as $column ) {
 	?>
-	<div class="single-album">
-		<div class="post-image">
-			<a href="<?php echo($permalink); ?>"><?php echo($post_img); ?></a>
+	<div class="pure-u-1-<?php echo($num_columns); ?>">
+	<?php 
+	foreach ( $column as $post ) {
+		$post_img = get_the_post_thumbnail($post->ID, 'album-list');
+		$permalink = get_permalink($post->ID);
+		?>
+		<div class="single-album">
+			<div class="post-image">
+				<a href="<?php echo($permalink); ?>"><?php echo($post_img); ?></a>
+			</div>
+			<div class="post-link">
+				<a href="<?php echo($permalink); ?>"><h3><?php echo($post->post_title); ?></h3></a>
+			</div>
 		</div>
-		<div class="post-link">
-			<a href="<?php echo($permalink); ?>"><h3><?php echo($post->post_title); ?></h3></a>
-		</div>
+		<?php		
+	} ?>
 	</div>
-	<?php
-
-} ?>
-
+<?php } ?>
+	</div>
 </div>
 
 
